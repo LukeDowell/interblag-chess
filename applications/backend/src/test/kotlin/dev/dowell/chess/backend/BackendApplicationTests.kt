98d95c46361644fde08059def432863185023d0b
= <=>
2019-11-13 10:24:10 -0600

    Adds spring backend

---
+++ applications/backend/src/test/kotlin/dev/dowell/chess/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.dowell.chess.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
